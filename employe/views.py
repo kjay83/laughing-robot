532ee9da59a4725f275364033a7da5b29fc25357
ruronikeWin10 <kabajewed@gmail.com>
2026-03-06 12:57:04 +0100

    debut code list.html cf. https://youtu.be/qRc0aeohMIg?si=0XVt6YZRpKenGti1&t=2271

--- employe/views.py
+++ employe/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Employe
 
 # Create your views here.
+
+def liste_employes(request):
+    employes = Employe.objects.all()
+    return render(request, 'employe/list.html' , {'employes' : employes})
