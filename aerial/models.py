2558c02a8edef7ef79b57c32ed7ebac078e13bbe
ruronikeWin10 <kabajewed@gmail.com>
2026-03-16 17:54:05 +0100

    recreation de la bdd suite aux modifs sur la bdd aerial

--- aerial/models.py
+++ aerial/models.py
@@ -21,36 +21,64 @@ class Player(models.Model):
     def pay(self,amount):
         self.money -= amount
 
-class Entreprise(models.Model):
+class CompagnieAerienne(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    nom = models.CharField(default="ENTREPRISE",max_length=200)
+    nom = models.CharField(default="AIR AERIAL",max_length=200)
     cash = models.DecimalField(default=0,max_digits=20, decimal_places=2)
     cash_flow = models.DecimalField(default=0,max_digits=20, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
-    type_entreprise =models.CharField(default="CLASSIQUE",max_length=200,blank=True,null=True)
+    type_entreprise =models.CharField(default="COMPANIE_AERIENNE",max_length=200,blank=True,null=True)
 
-class CompagnieAerienne(Entreprise):
-    def __init__(self):
-        self.type_entreprise="COMPANIE_AERIENNE"
+    def __str__(self):
+        return f"{self.nom}"
 
-    def faire_voler_flotte(self):
-        self.cash += 10
+#TODO: faire heriter les types d'entreprise de la classe entreprise
+# #class CompagnieAerienne(Entreprise):
+    #def __init__(self):
+       # self.type_entreprise="COMPANIE_AERIENNE"
+
+   # def faire_voler_flotte(self):
+    #    self.cash += 10
 
 class modeleAvion(models.Model):
     nom = models.CharField(max_length=50)
+    fabricant = models.CharField(max_length=50,blank=True,null=True)
     nb_places = models.IntegerField(default=200,blank=True)
-    nb_heures_max = models.FloatField(default=48,blank=True)
-    nb_heures_maintenance = models.FloatField(default=1,blank=True)
+    nb_km_max_par_vol = models.IntegerField(default=3000,blank=True)
+    nb_km_max_par_exploitation = models.IntegerField(default=10000,blank=True)
+    nb_km_maintenance = models.IntegerField(default=100,blank=True) 
+    nb_heures_maintenance = models.DecimalField(default=1,max_digits=5, decimal_places=2,blank=True)
     maintenance_fee_percentage = models.IntegerField(default=10,blank=True)
     prix_achat = models.DecimalField(default=100,max_digits=20, decimal_places=2)
 
     def get_couts_maintenance(self) -> float:
         return self.prix_achat * self.maintenance_fee_percentage * self.nb_heures_maintenance
+    
+    def __str__(self):
+        return f"{self.fabricant}_{self.nom}"
 
 
 class Avion(models.Model):
     compagnie = models.ForeignKey(CompagnieAerienne, on_delete=models.CASCADE)
     modele = models.ForeignKey(modeleAvion, on_delete=models.CASCADE)
+    nb_heures_fonctionnement = models.DecimalField(default=0,max_digits=5, decimal_places=2,blank=True)
+    est_a_maintenir = models.BooleanField(default=False,blank=True)
+    est_amorti = models.BooleanField(default=False,blank=True)
+    km_parcourus = models.IntegerField(default=0,blank=True)
+    #numero de l'avion dans la flotte
+    indicatif_flotte = models.IntegerField(default=0,blank=True)
 
+    def verifie_amortissement(self) :
+        if self.nb_heures_fonctionnement == self.modele.nb_km_max_par_exploitation :
+            self.est_amorti = True
+    
+    def verifie_maintenance(self):
+        if self.km_parcourus % self.modele.nb_km_maintenance == 0:
+            self.est_a_maintenir = True
+        
+    def faire_maintenance(self):
+        self.est_a_maintenir = False
 
+    def __str__(self):
+        return f"{self.compagnie}_{self.modele}_{self.indicatif}"
 
